fb2353614b26fb6325b299afd19f454cd3472a76
Chr0n0sXIII <71567732+Chr0n0sXIII@users.noreply.github.com>
2021-11-15 11:23:04 -0400

    Update FileCopier.java

--- src/FileCopier.java
+++ src/FileCopier.java
@@ -4,26 +4,42 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import java.util.stream.Stream;
+import java.io.File;
+import java.io.FileFilter;
 
 public class FileCopier {
 
     private String source;
     private String destination;
+    private FileFilter filter;
 
     public FileCopier(String source, String destination){
         this.destination = destination;
         this.source = source;
+        filter = new FileFilter() {
+
+            @Override
+            public boolean accept(File file) {
+                if (file.getName().endsWith(".pdf")) {
+                    return true;
+                 }
+                 return false;
+            }
+            
+        };
     }
 
     public void copier () throws IOException{
         Path src  = Paths.get(source);
         Path dest = Paths.get(destination);
 
-        Stream<Path> files = Files.walk(src);
+        Stream<Path> files = Files.walk(src,1);
 
+        
 
         files.forEach(file -> {
             try {
+                if(filter.accept(file.toFile()))
                 Files.copy(file, dest.resolve(src.relativize(file)),
                         StandardCopyOption.REPLACE_EXISTING);
             }
