6af054b1c746df66b8cbc715529bc7e9d9929116
Chr0n0sXIII <71567732+Chr0n0sXIII@users.noreply.github.com>
2021-11-17 14:45:39 -0400

    fixed syntax

---
+++ comp3607/src/main/StudentData.java
@@ -0,0 +1,99 @@
+public class StudentData {
+
+    private String identifier;
+    private String fullName;
+    private String IDNumber;
+    private String email;
+    private String status;
+    private String grade;
+    private String maximumGrade;
+    private String changeStatus;
+    private String lastModified;
+    private String feedback;
+
+    public StudentData(int ID, String Name){
+        setIDNumber(Integer.toString(ID));
+        setFullName(Name);
+    }
+
+    public String getIdentifier() {
+        return identifier;
+    }
+
+    public void setIdentifier(String identifier) {
+        this.identifier = identifier;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getIDNumber() {
+        return IDNumber;
+    }
+
+    public void setIDNumber(String iDNumber) {
+        IDNumber = iDNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getGrade() {
+        return grade;
+    }
+
+    public void setGrade(String grade) {
+        this.grade = grade;
+    }
+
+    public String getMaximumGrade() {
+        return maximumGrade;
+    }
+
+    public void setMaximumGrade(String maximumGrade) {
+        this.maximumGrade = maximumGrade;
+    }
+
+    public String getChangeStatus() {
+        return changeStatus;
+    }
+
+    public void setChangeStatus(String changeStatus) {
+        this.changeStatus = changeStatus;
+    }
+
+    public String getLastModified() {
+        return lastModified;
+    }
+
+    public void setLastModified(String lastModified) {
+        this.lastModified = lastModified;
+    }
+
+    public String getFeedback() {
+        return feedback;
+    }
+
+    public void setFeedback(String feedback) {
+        this.feedback = feedback;
+    }
+
+}
