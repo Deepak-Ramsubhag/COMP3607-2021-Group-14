9adeb938dbe9c329a0374cf94ebead0c5c2e7eba
Chr0n0sXIII <71567732+Chr0n0sXIII@users.noreply.github.com>
2021-11-21 19:06:11 -0400

    test update

--- demo/src/test/java/com/example/AppTest.java
+++ demo/src/test/java/com/example/AppTest.java
@@ -2,17 +2,22 @@ package com.example;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
-/**
- * Unit test for simple App.
- */
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
 class AppTest {
-    /**
-     * Rigorous Test.
-     */
+    //Uzip Variables
+    String zipsource = "C:/Users/jiles/Documents/GitHub/COMP3607-2021-Group-14/filesToRename/sample3 case_i.zip";
+    String zipdest = "C:/Users/jiles/Documents/GitHub/COMP3607-2021-Group-14/filesToRename/";
+    UnzipFiles uzo = new UnzipFiles();
+    
     @Test
-    void test() {
-        assertEquals(1, 1);
+    void testUnzip() {
+        boolean test = uzo.unzip(zipsource, zipdest);
+        
+        assertTrue(test);
     }
+
+    
 }
